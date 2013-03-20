2201c66c07370c3d67e4d265a58b5a1f2158ecb6
Izzy <gisrael@seas.upenn.edu>
2013-03-19 20:58:53 -0400

    All registration pages other than avatars. Contains a few bugs. When adding new appointments, couldn't get scrollview working for some reason so it pushed the next buttons off screen.

--- MedicineReminder/src/com/example/medicinereminder/MainActivity.java
+++ MedicineReminder/src/com/example/medicinereminder/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.medicinereminder;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.sql.Date;
 
 import android.os.Bundle;
 import android.annotation.SuppressLint;
@@ -48,10 +49,10 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
-	
+
 	public void showDatePickerDialog(View v) {
-	    DialogFragment newFragment = new DatePickerFragment();
-	    newFragment.show(getFragmentManager(), "datePicker");
+		DialogFragment newFragment = new DatePickerFragment();
+		newFragment.show(getFragmentManager(), "datePicker");
 	}
 
 	public static class DatePickerFragment extends DialogFragment
@@ -73,12 +74,12 @@ public class MainActivity extends Activity {
 			date = Integer.toString(year)+Integer.toString(month)+Integer.toString(day);
 		}
 	}
-	
+
 	public void toNextPage(View v){
 		ViewFlipper flip = (ViewFlipper) findViewById(R.id.view_flipper);
 		flip.showNext();
 	}
-	
+
 	public void sumbitForm(View v){
 		String firstName = ((EditText) findViewById(R.id.EditFirstName)).getText().toString();
 		String lastName = ((EditText) findViewById(R.id.EditLastName)).getText().toString();
@@ -86,51 +87,83 @@ public class MainActivity extends Activity {
 		String viralLoad = ((EditText) findViewById(R.id.EditViralLoad)).getText().toString();
 		String phone = ((EditText) findViewById(R.id.EditPhone)).getText().toString();
 		String provider = ((EditText) findViewById(R.id.EditProviderPhone)).getText().toString();
-		
+
 		ArrayList<String> medicineNames = new ArrayList<String>();
 		LinearLayout medicines = (LinearLayout)findViewById(R.id.medicineBoxes);
 		for( int i = 0; i<medicines.getChildCount(); i++ )
-		    medicineNames.add(((EditText) medicines.getChildAt(i)).getText().toString());
-		
+			medicineNames.add(((EditText) medicines.getChildAt(i)).getText().toString());
+
 		ArrayList<String> medicineTimes = new ArrayList<String>();
 		LinearLayout times = (LinearLayout)findViewById(R.id.timeBoxes);
 		for( int i = 0; i<times.getChildCount(); i++ )
-		    medicineTimes.add(((EditText) times.getChildAt(i)).getText().toString());
-		
+			medicineTimes.add(((EditText) times.getChildAt(i)).getText().toString());
+
 		int remindertime = ((NumberPicker) findViewById(R.id.np)).getValue();
 		AlarmSet alarmset = new AlarmSet(this);
 		alarmset.setAlarm(medicineTimes.get(0), remindertime);
-		
+
 		String custommessage = ((EditText) findViewById(R.id.EditReminderMessage)).getText().toString();
 		
+		//TODO Need to removed deprecated constructor for Date. 
+		ArrayList<Date> appointments = new ArrayList<Date>();
+		LinearLayout apps = (LinearLayout)findViewById(R.id.appointmentBoxes);
+		for( int i = 0; i<times.getChildCount(); i++ ){
+			DatePicker dp = (DatePicker) apps.getChildAt(i);
+					appointments.add(new Date(dp.getYear(),dp.getMonth(),dp.getDayOfMonth()));
+		}
+		
+		//TODO Need to removed deprecated constructor for Date.
+		ArrayList<Date> refills = new ArrayList<Date>();
+		LinearLayout refs = (LinearLayout)findViewById(R.id.refillBoxes);
+		for( int i = 0; i<refs.getChildCount(); i++ ){
+			DatePicker dp = (DatePicker) refs.getChildAt(i);
+					refills.add(new Date(dp.getYear(),dp.getMonth(),dp.getDayOfMonth()));
+		}
+
 		Intent intent = new Intent(this, HomePageActivity.class);
 		startActivity(intent);
-		
-		
+
+
 		//Set Static User Attributes based off submission form
 		MyGuy.getUser().setFirstName(firstName);
 		MyGuy.getUser().setLastName(lastName);
 		MyGuy.getUser().setPhoneNumer(phone);
 		MyGuy.getUser().setAlarmMessage(custommessage);
 	}
-	
-	
+
+
 	public void toPrevPage(View v){
 		ViewFlipper flip = (ViewFlipper) findViewById(R.id.view_flipper);
 		flip.showPrevious();
 	}
-	
+
 	public void addMedicineBox(View v){
 		LinearLayout medicines = (LinearLayout) findViewById(R.id.medicineBoxes);
 		EditText medicinebox = new EditText(this);
 		medicinebox.setText("Medicine Name");
 		medicines.addView(medicinebox);
-		
+
 		LinearLayout times = (LinearLayout) findViewById(R.id.timeBoxes);
 		EditText timebox = new EditText(this);
 		timebox.setText("Time");
 		timebox.setInputType(InputType.TYPE_DATETIME_VARIATION_TIME);
 		times.addView(timebox);
 	}
-	
+
+	public void addAppointmentBox(View v){
+		LinearLayout appointments = (LinearLayout) findViewById(R.id.appointmentBoxes);
+		DatePicker appbox = new DatePicker(this);
+		appbox.setCalendarViewShown(false);
+		appointments.addView(appbox);
+	}
+
+	public void addRefillBox(View v){
+		LinearLayout refills = (LinearLayout) findViewById(R.id.refillBoxes);
+		DatePicker refbox = new DatePicker(this);
+		refbox.setCalendarViewShown(false);
+		refills.addView(refbox);
+	}
+
+
+
 }
