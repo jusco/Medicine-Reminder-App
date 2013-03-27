78f2c5d9acadc004c718e716c227eb34656a7c2f
Justin <jusco@seas.upenn.edu>
2013-03-26 21:42:42 -0400

    Revert "Spinner Added to choose sleep time"
    This reverts commit 6747a37034bc5dbebccbbbcb9f14cf3d6afbb4bb.

--- MedicineReminder/src/com/example/medicinereminder/AlarmPage.java
+++ MedicineReminder/src/com/example/medicinereminder/AlarmPage.java
@@ -6,57 +6,26 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 public class AlarmPage extends Activity {
 	AlarmSet alarmSet;
-	String time;
 	 @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		 super.onCreate(savedInstanceState);
 		 setContentView(R.layout.alarm_page);
 		 TextView out = (TextView)findViewById(R.id.alarmMessage);
 		 out.append(MyGuy.getUser().alarmMessage);
-		 
-		 Spinner spinner = (Spinner) findViewById(R.id.editSleep);
-		 // Create an ArrayAdapter using the string array and a default spinner layout
-		 ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-		        R.array.sleep_array, android.R.layout.simple_spinner_item);
-		 // Specify the layout to use when the list of choices appears
-		 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		 // Apply the adapter to the spinner
-		 spinner.setAdapter(adapter);
-		
-		 spinner.setOnItemSelectedListener(new SpinnerActivity());
+		 alarmSet = new AlarmSet(this);
 		 MediaPlayer mp = MediaPlayer.create(getBaseContext(),R.raw.lickshot);
 		 mp.start();
 	 }
 	 
-	 
-	 public class SpinnerActivity extends Activity implements OnItemSelectedListener {
-		
-		    
-		 public void onItemSelected(AdapterView<?> parent, View view, 
-				 int pos, long id) {
-			 	// An item was selected. You can retrieve the selected item using
-			 	time =(String)parent.getItemAtPosition(pos);
-		 }
-
-		    public void onNothingSelected(AdapterView<?> parent) {
-		        time = "5";
-		    }
-		}
-	 
 	 public void onIgnoreButtonClick(View view){
-		 //EditText times = (EditText)findViewById(R.id.editSleep);
-		 alarmSet = new AlarmSet(this);
-		 alarmSet.setSleep(time);
+		 EditText times = (EditText)findViewById(R.id.editSleep);
+		 alarmSet.setSleep(times.getText().toString());
 		 finish();
 	 }
 	 
